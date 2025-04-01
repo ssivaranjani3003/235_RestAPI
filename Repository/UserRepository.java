6e953e3255c78ef4d245be014ca3463498c27ec8
ssivaranjani3003 <sivaranjanipriya3020@gmail.com>
2025-04-01 19:16:49 +0530

    Delete Repository/UserRepository.java

--- Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
